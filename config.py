2992165e7c18914adb02611dcd4625fa45789973
周超 <zc98770@alibaba-inc.com>
2018-03-12 18:50:42 +0800

    data view

---
+++ config.py
@@ -0,0 +1,7 @@
+# train data
+round1_train_file_path = './data/round1_ijcai_18_train_20180301.txt'
+
+# test data
+round1_test_file_path = './data/round1_ijcai_18_test_a_20180301.txt'
+
+# feature data

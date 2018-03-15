9663c13c4ee4efb18da2cc13eaaeb74b94db15e9
周超 <zc98770@alibaba-inc.com>
2018-03-15 13:39:18 +0800

    base line train day 09-19 ~ 09-24 valid day 09-18 test day  09-25 eval-logloss:0.099489

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
-# train data
+# origin train data & test data
 round1_train_file_path = './data/round1_ijcai_18_train_20180301.txt'
-
-# test data
 round1_test_file_path = './data/round1_ijcai_18_test_a_20180301.txt'
 
-# feature data
+# feature train data & test data
+train_valid_features_file_path = './temp/train_valid_features.csv'
+test_valid_features_file_path = './temp/test_valid_features.csv'
+train_data_features_file_path = './temp/train_data_features.csv'
+test_data_features_file_path = './temp/test_data_features.csv'
+
+# result data
+model_path = './result/model/model'
+model_train_log = 'train.log'
+model_valid_log = 'valid.log'
+result_filename = 'result.csv'

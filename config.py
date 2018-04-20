41479715d5f1041ccb17db90a4ab9f109df5b991
周超 <zc98770@alibaba-inc.com>
2018-04-20 19:22:04 +0800

    b 榜baseline

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # origin train data & test data
 round1_train_file_path = './data/round1_ijcai_18_train_20180301.txt'
-round1_test_file_path = './data/round1_ijcai_18_test_a_20180301.txt'
+round1_test_a_file_path = './data/round1_ijcai_18_test_a_20180301.txt'
+round1_test_b_file_path = './data/round1_ijcai_18_test_b_20180418.txt'
 
 # feature train data & test data
 train_valid_features_file_path = './temp/train_valid_features.csv'
